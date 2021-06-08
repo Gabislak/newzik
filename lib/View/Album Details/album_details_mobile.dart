e74e959e37e6d148e67680744cacac106a982fb5
Gabriel <gabionascu@gmail.com>
2021-06-08 12:39:20 +0100

    Add new Desktop Album Details View

--- lib/View/Album Details/album_details_mobile.dart
+++ lib/View/Album Details/album_details_mobile.dart
@@ -13,73 +13,82 @@ class AlbumDetailsMobile extends StatelessWidget {
     return ViewModelBuilder<AlbumDetailsViewModel>.reactive(
       viewModelBuilder: () => AlbumDetailsViewModel(),
       onModelReady: (model) => model.getOneAlbum(id),
-      builder: (context, model, child) => Padding(
-        padding: EdgeInsets.symmetric(
-            vertical: MediaQuery.of(context).size.height * 0.05,
-            horizontal: MediaQuery.of(context).size.width * 0.00),
-        child: model.album == null
-            ? CircularProgressIndicator()
-            : Column(
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  Card(
-                    elevation: 20,
-                    clipBehavior: Clip.antiAlias,
-                    child: Image.network(
-                      model.album.coverMedium,
-                      fit: BoxFit.cover,
-                    ),
-                  ),
-                  SizedBox(
-                    height: 20,
-                  ),
-                  Text(
-                    model.album.album,
-                    style: TextStyle(
-                        fontSize: 21, color: highEmphasisTextOnSurface),
-                    softWrap: true,
-                  ),
-                  Text(
-                    'by ${model.album.artist}',
-                    style: TextStyle(
-                        fontSize: 17, color: mediumEmphasisTextOnSurface),
-                    softWrap: true,
-                  ),
-                  SizedBox(
-                    height: 20,
-                  ),
-                  Wrap(
-                    //direction: Axis.vertical,
-                    //runSpacing: 2,
-                    //spacing: 2,
+      builder: (context, model, child) => Scrollbar(
+        isAlwaysShown: true,
+        child: ListView(
+          padding: EdgeInsets.symmetric(
+              vertical: MediaQuery.of(context).size.height * 0.05,
+              horizontal: MediaQuery.of(context).size.width * 0.00),
+          children: <Widget>[
+            model.album == null
+                ? CircularProgressIndicator()
+                : Column(
+                    crossAxisAlignment: CrossAxisAlignment.center,
                     children: [
-                      AlbumElementCard(
-                        header: "Note",
-                        content:
-                            '${model.album.totalRating.toStringAsFixed(1)}',
-                        subtitle: "/ 10",
+                      Card(
+                        elevation: 20,
+                        clipBehavior: Clip.antiAlias,
+                        child: Image.network(
+                          model.album.coverMedium,
+                          fit: BoxFit.cover,
+                        ),
                       ),
-                      AlbumElementCard(
-                        header: "Noté par",
-                        content: '${model.album.nbRatings.toStringAsFixed(0)}',
-                        subtitle: "/ 5",
+                      SizedBox(
+                        height: 20,
                       ),
-                      AlbumElementCard(
-                          header: "Sortit en",
-                          content:
-                              '${model.album.releaseDate.substring(0, 4)}'),
-                      AlbumElementCard(
-                        header: "Durée",
-                        content:
-                            '${(model.album.duration / 60).roundToDouble()}',
-                        subtitle: "minutes",
+                      Text(
+                        model.album.album,
+                        style: TextStyle(
+                            fontSize: 21, color: highEmphasisTextOnSurface),
+                        softWrap: true,
                       ),
-                      AlbumElementCard(
-                          header: "Genre", content: '${model.album.genre}'),
+                      Text(
+                        'by ${model.album.artist}',
+                        style: TextStyle(
+                            fontSize: 17, color: mediumEmphasisTextOnSurface),
+                        softWrap: true,
+                      ),
+                      SizedBox(
+                        height: 20,
+                      ),
+                      Wrap(
+                        //direction: Axis.vertical,
+                        //runSpacing: 2,
+                        //spacing: 2,
+                        children: [
+                          AlbumElementCard(
+                            header: "Note",
+                            content:
+                                '${model.album.totalRating.toStringAsFixed(1)}',
+                            subtitle: "/ 10",
+                          ),
+                          AlbumElementCard(
+                            header: "Noté par",
+                            content:
+                                '${model.album.nbRatings.toStringAsFixed(0)}',
+                            subtitle: "/ 5",
+                          ),
+                          AlbumElementCard(
+                              header: "Sortit en",
+                              content:
+                                  '${model.album.releaseDate.substring(0, 4)}'),
+                          AlbumElementCard(
+                            header: "Durée",
+                            content:
+                                '${(model.album.duration / 60).roundToDouble()}',
+                            subtitle: "minutes",
+                          ),
+                          AlbumElementCard(
+                              header: "Genre", content: '${model.album.genre}'),
+                          AlbumElementCard(
+                              header: "Nb de Morceaux",
+                              content: '${model.album.nbTracks}'),
+                        ],
+                      ),
                     ],
                   ),
-                ],
-              ),
+          ],
+        ),
       ),
     );
   }
